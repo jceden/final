f1a3bd3b1f3e5ad3fc63b67c8170508548ec71ac
jceden <jeden1@mail.csuchico.edu>
2014-12-12 06:21:14 +0000

    site working, at least in development

--- cute/db/seeds.rb
+++ cute/db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create! :username => 'Arnold', :email => 'a@a.com', :password => 'a', :password_confirmation => 'a'
+User.create! :username => 'CatL0000ver', :email => 'c@c.com', :password => 'c', :password_confirmation => 'c'
+User.create! :username => 'Lupine', :email => 'd@d.com', :password => 'd', :password_confirmation => 'd'
+User.create! :username => 'BarryTOne', :email => 'b@b.com', :password => 'b', :password_confirmation => 'b'
+
+Image.create! :filename => 'CALEIFHANLKFJSI.jpg', :img_type => 'Rodent', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'CFBGXDRHPQBHIXJ.jpg', :img_type => 'Rodent', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'CKEIREWINEPXIEE.jpg', :img_type => 'Rodent', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'CPFKJUYIHNMZAKF.jpg', :img_type => 'Rodent', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'CPPOLKIUYHNBGFV.jpg', :img_type => 'Rodent', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'DPLGKHIXEEPTNUH.jpg', :img_type => 'Dog', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'DQIYOPUINNTTQAS.jpg', :img_type => 'Dog', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'GUYZGFMABNJUDVU.jpg', :img_type => 'Cat', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'HPCSAARMUXPPWBB.jpg', :img_type => 'Hedgehog', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'HSFAHWWJYJCCCXF.jpg', :img_type => 'Dog', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'MJXFNZTGRKTUNRB.jpg', :img_type => 'Bird', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'NRBMCWLFLWMBIFC.jpg', :img_type => 'Reptile', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'PSDJQKRRZCKICIG.jpg', :img_type => 'Baby', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'QFIVVUCCOMTSHOM.jpg', :img_type => 'Bird', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'WVNBOXGZFXQTHSG.jpg', :img_type => 'Dog', :user_id => 1,:total_vote => 0,:cute_vote=>0
+Image.create! :filename => 'ZDTICQSNBMDZMRV.jpg', :img_type => 'Horse', :user_id => 1,:total_vote => 0,:cute_vote=>0
